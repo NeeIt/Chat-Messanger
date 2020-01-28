933fef380457c0c26edf78bc38d2f05f167d590a
Nee <vushmankin@gmail.com>
2020-01-28 21:17:56 +0300

    Bitch lazania

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -3,11 +3,14 @@ import './Message.scss';
 import PropTypes from 'prop-types';
 import {formatDistance,subDays,formatRelative } from 'date-fns' ;
 import ruLocele from 'date-fns/locale/ru';
+import classNames from 'classnames';
+import NotCheckedIco from 'assets/icos/tick.svg';
+import CheckedIco from 'assets/icos/double_tick.png'; 
 
 export default class Message extends React.Component {
     render() {
         return (
-            <div className="message">
+            <div className={classNames("message",(this.props.isMe)?'me':'notMe')}>
                 <div className="message__avatar">
                     <img src={this.props.avatar} alt="user Avatar" />
                 </div>
@@ -15,9 +18,25 @@ export default class Message extends React.Component {
                     <div className="message__bubble">
                         {this.props.text}
                     </div>
+                    {this.props.attachments && this.props.attachments.imgs?
+                    <div className="message__attachmentImgsContainer">
+                    {
+                    this.props.attachments.imgs.map(
+                        (img,id)=>
+                        <div className="message__attachmentImg" id={id}>
+                            <img src={img.src} alt={"attachment image #"+id} />
+                        </div>)
+                    }
+                    </div>
+                        
+                        :false}
+                
                     <span className="message__date">
                         {formatDistance(this.props.date,new Date(),{ addSuffix: true,locale:ruLocele })}
                     </span>
+                </div>
+                <div className="message__checked">
+                   <img src={(this.props.isReaded)?CheckedIco:NotCheckedIco} alt={(this.props.isReaded)?"Прочтено":"Не прочтено"}/>
                 </div>
             </div>
         );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,23 +10,58 @@ export default class Home extends React.Component{
                 <Message 
                     avatar={"https://sun9-61.userapi.com/c206624/v206624712/2cc4c/0zQkIX0pM1k.jpg?ava=1"}
                     text="Hello. I know You do not know me. But I wish you happiness 😀"
-                    date={new Date(2019,12,20)} />
+                    isReaded={true}
+                    date={new Date(2019,11,20)} />
                     <Message 
                     avatar={"https://sun9-61.userapi.com/c206624/v206624712/2cc4c/0zQkIX0pM1k.jpg?ava=1"}
                     text="StressTest StressTest StressTest StressTest StressTest StressTest StressTest StressTest StressTest StressTest StressTest StressTest StressTest StressTest StressTest "
-                    date={new Date(2019,12,22)} />
+                    isReaded={true}
+                    date={new Date(2019,11,22)} />
                     <Message 
                     avatar={"https://sun9-61.userapi.com/c206624/v206624712/2cc4c/0zQkIX0pM1k.jpg?ava=1"}
                     text="StressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTest StressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTest StressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTestStressTest"
-                    date={new Date(2019,12,27)} />
+                    isReaded={true}
+                    attachments={
+                        {
+                            imgs:[
+                                {src:'https://picsum.photos/200/300?random=9'},
+                                {src:'https://picsum.photos/200/300?random=8'},
+                                {src:'https://picsum.photos/200/300?random=7'},
+                                {src:'https://picsum.photos/200/300?random=6'},
+                                {src:'https://picsum.photos/200/300?random=5'},
+                                {src:'https://picsum.photos/200/300?random=4'},
+                                {src:'https://picsum.photos/200/300?random=3'},
+                                {src:'https://picsum.photos/200/300?random=2'},
+                                {src:'https://picsum.photos/200/300?random=1'}
+                            ]
+                        }
+                    }
+                    date={new Date(2019,11,27)} />
                     <Message 
                     avatar={"https://sun9-61.userapi.com/c206624/v206624712/2cc4c/0zQkIX0pM1k.jpg?ava=1"}
                     text="What is cooking, good looking?"
-                    date={new Date(2019,12,30)} />
+                    attachments={
+                        {
+                            imgs:[{src:'https://source.unsplash.com/random'}]
+                        }
+                    }
+                    date={new Date(2019,11,30)} />
                     <Message 
                     avatar={"https://sun9-61.userapi.com/c206624/v206624712/2cc4c/0zQkIX0pM1k.jpg?ava=1"}
                     text="A"
-                    date={new Date(2020,1,1)} />
+                    date={new Date(2020,0,1)}
+                    isMe={true}
+                    attachments={
+                        {
+                            imgs:[
+                                {src:'https://picsum.photos/200/300?random=13'},
+                                {src:'https://picsum.photos/200/300?random=12'},
+                                {src:'https://picsum.photos/200/300?random=11'},
+                                {src:'https://picsum.photos/200/300?random=14'}
+                            ]
+                        }
+                    }
+                    />
                 </div>
             </section>
         )
